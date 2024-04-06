f5e3368d31928d3d14780fc55b71418927d1343f
Jephthah Jehosaphat Landicho <landichojehosaphat@gmail.com>
2024-04-06 00:49:25 +0000

    typing effect

--- assets/script.js
+++ assets/script.js
@@ -177,3 +177,42 @@ themeButton.addEventListener("click", () => {
   localStorage.setItem("selected-theme", getCurrentTheme());
   localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+
+const phrases = ["Web Developer", "Database Administrator", "Data Analyst", "Freelancer"];
+let currentPhrase = 0;
+let currentLetter = 0;
+let isDeleting = false;
+const typingSpeed = 200;
+const deletingSpeed = 100;
+const betweenPhraseDelay = 2000; // 2 seconds delay between phrases
+const element = document.getElementById('serviceDisplay');
+
+function type() {
+  const current = phrases[currentPhrase];
+  let displayText;
+
+  if (!isDeleting) {
+    displayText = current.substring(0, currentLetter++);
+    if (currentLetter === current.length + 1) {
+      isDeleting = true;
+      setTimeout(type, betweenPhraseDelay); // Pause at the end of a phrase
+    } else {
+      setTimeout(type, typingSpeed);
+    }
+  } else {
+    displayText = current.substring(0, currentLetter--);
+    if (currentLetter === 0) {
+      isDeleting = false;
+      currentPhrase = (currentPhrase + 1) % phrases.length; // Loop back to first phrase
+      setTimeout(type, typingSpeed);
+    } else {
+      setTimeout(type, deletingSpeed);
+    }
+  }
+
+  element.textContent = displayText;
+}
+
+// Start the typing effect
+type();
